13e3f2de62874c452aeb0fc02e4fcd6804f84506
SuthaweeMek <suthawee.w@gmail.com>
2019-11-21 10:29:13 +0700

    Change function program1

--- Lottery.c
+++ Lottery.c
@@ -10,7 +10,7 @@ void main()
 
   printf("Enter your Lottery Number (0 to end): ");
   scanf("%d", &YourLotteryNo);
-  while (YourLotteryNo != 0)
+  while (YourLotteryNo != 3)
   {
     won = 0;
     for (i=0;i<N && !won; i++)
